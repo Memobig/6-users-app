0afc803d66f639c8b7884a1926dc815a4b1c8d07
Memobig <memobig@gmail.com>
2024-05-11 18:48:11 -0600

    mensajes de alerta con sweetalert

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Swal from "sweetalert2";
 
 export const UserForm = ({ handlerAddUser, userSelected, initialUserForm }) => {
 
@@ -8,7 +9,7 @@ export const UserForm = ({ handlerAddUser, userSelected, initialUserForm }) => {
     useEffect(() => {
         setUserForm({
             ...userSelected,
-            password:''
+            password: ''
         })
     }, [userSelected]);
 
@@ -25,7 +26,11 @@ export const UserForm = ({ handlerAddUser, userSelected, initialUserForm }) => {
         event.preventDefault();
 
         if (!username || (!password && id === 0) || !email) {
-            alert('Debe completar los campos del formulario');
+            Swal.fire({
+                title: "Error de validación!",
+                text: "Debe completar los campos del formulario",
+                icon: "error"
+            });
             return;
         }
 
@@ -45,29 +50,29 @@ export const UserForm = ({ handlerAddUser, userSelected, initialUserForm }) => {
                 value={username}
                 onChange={onInputChange} />
 
-                {id > 0 || <input
+            {id > 0 || <input
                 className="form-control my-3 w-75"
                 placeholder="Password"
                 type="password"
                 name="password"
                 value={password}
                 onChange={onInputChange} />}
-            
+
             <input
                 className="form-control my-3 w-75"
                 placeholder="Email"
                 name="email"
                 value={email}
                 onChange={onInputChange} />
-                <input
+            <input
                 type="hidden"
                 name="id"
-                value={id}/>
+                value={id} />
             <button
                 className="btn btn-primary"
                 type="submit">
-                    {id > 0 ? 'Editar': 'Crear'}
-                </button>
+                {id > 0 ? 'Editar' : 'Crear'}
+            </button>
         </form>
     );
 }
--- src/hooks/useUsers.js
+++ src/hooks/useUsers.js
@@ -1,5 +1,6 @@
 import { useReducer, useState } from "react";
 import { usersReducers } from "../reducers/usersReducers";
+import Swal from "sweetalert2";
 
 const initialUsers = [
     {
@@ -35,13 +36,43 @@ export const useUsers = () => {
             type: type,
             payload: user,
         });
+
+        Swal.fire(
+            (user.id === 0)? 
+            "Usuario Creado" :
+             "Usuario Actualizado",
+            (user.id === 0)? 
+            "El usuario ha sido creado con exito!":
+            "El usuario ha sido actualizado con exito!",
+            "success"
+          );
     }
 
     const handlerRemoveUser = (id) => {
-        dispatch({
-            type: 'removeUser',
-            payload: id,
-        });
+
+        Swal.fire({
+            title: "¿Estas seguro?",
+            text: "No podras revertir esta acción",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Sí, eliminar!"
+          }).then((result) => {
+            if (result.isConfirmed) {
+
+                dispatch({
+                    type: 'removeUser',
+                    payload: id,
+                });
+
+              Swal.fire({
+                title: "Usuario Eliminado!",
+                text: "El usuario ha sido eliminado con exito!",
+                icon: "success"
+              });
+            }
+          });
     }
 
     const handlerUserSelectForm = (user) => {
