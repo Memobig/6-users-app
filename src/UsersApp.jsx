d5bc0dae54367a6dbf246dfedca4834d9dfec5ee
Memobig <memobig@gmail.com>
2024-05-21 00:00:30 -0600

    nuevo componente modal

--- src/UsersApp.jsx
+++ src/UsersApp.jsx
@@ -1,4 +1,5 @@
 import { UserForm } from "./components/UserForm";
+import { UserModalForm } from "./components/UserModalForm";
 import { UsersList } from "./components/UsersList";
 import { useUsers } from "./hooks/useUsers";
 
@@ -21,28 +22,11 @@ export const UsersApp = () => {
     return (
         <>
             {!visibleForm ||
-                <div className="abrir-modal animacion fadeIn">
-                    <div className="modal" style={{display:"block"}} tabIndex="-1">
-                        <div className="modal-dialog" role="document">
-                            <div className="modal-content">
-                                <div className="modal-header">
-                                    <h5 className="modal-title">
-                                        {userSelected.id > 0 ? 'Editar' : 'Crear'} Modal Usuario
-                                    </h5>
-                                </div>
-                                <div className="modal-body">
-                                    <UserForm
-                                        handlerAddUser={handlerAddUser}
-                                        initialUserForm={initialUserForm}
-                                        userSelected={userSelected}
-                                        handlerCloseForm={handlerCloseForm}
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
+                <UserModalForm 
+                handlerAddUser={handlerAddUser}
+                initialUserForm={initialUserForm}
+                userSelected={userSelected}
+                handlerCloseForm={handlerCloseForm}/>
             }
             <div className="container my-4">
                 <h2>Users App</h2>
