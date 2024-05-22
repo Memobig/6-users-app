f69bb073e11fff7f20ab7ce25407294033a57599
Memobig <memobig@gmail.com>
2024-05-22 00:28:58 -0600

    agregando funcionalidad al login

--- src/UsersApp.jsx
+++ src/UsersApp.jsx
@@ -1,14 +1,32 @@
+import { useReducer, useState } from "react";
 import { LoginPages } from "./auth/pages/LoginPage";
 import { UsersPage } from "./pages/UserPages";
+import { loginReducer } from "./auth/reducers/loginReducer";
+import Swal from "sweetalert2";
 
-
+const initialLogin = {
+    isAuth: false,
+    user: undefined,
+}
 
 export const UsersApp = () => {
 
+    const [login, dispatch] = useReducer(loginReducer,initialLogin);
+    const handlerLogin = ({username, password}) => {
+        if(username ==='admin' && password === '12345'){
+            const user = {username: 'admin'};
+            dispatch({
+                type: 'login',
+                payload: user,
+            })
+        } else {
+            Swal.fire("Error login",'Username o password invalidos', 'error');
+        }
+    }
+
     return (
         <>
-        <LoginPages/>
-        {/*<UsersPage/>*/}
+        {login.isAuth ? <UsersPage/> : <LoginPages handlerLogin={handlerLogin}/>}
         </>
     );
 }
--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,4 +1,34 @@
-export const LoginPages = () => {
+import { useState } from "react";
+import Swal from "sweetalert2";
+
+const initialLoginForm = {
+    username: '',
+    password: '',
+}
+
+export const LoginPages = ({handlerLogin}) => {
+    const [loginForm, setLoginForm] = useState(initialLoginForm);
+    const {username, password} = loginForm;
+
+    const onInputChange = ({target}) => {
+        const {name, value} = target;
+        setLoginForm({
+            ...loginForm,
+            [name] : value,
+        })
+    }
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if(!username || !password){
+            Swal.fire("Error de validacion",'Username y password requeridos', 'error');
+        }
+
+        //aca se implementa el login
+        handlerLogin({username, password});
+
+        setLoginForm(initialLoginForm);
+    }
+
     return (
         <div className="modal" style={{display:"block"}} tabIndex="-1">
             <div className="modal-dialog">
@@ -6,16 +36,21 @@ export const LoginPages = () => {
                     <div className="modal-header">
                         <h5 className="modal-title">Login Page</h5>
                     </div>
-                    <form>
+                    <form onSubmit={onSubmit}>
                         <div className="modal-body">
                             <input 
                             className="form-control my-3 w-75"
                             placeholder="Username"
-                            name="username"/>
+                            name="username"
+                            value={username}
+                            onChange={onInputChange}/>
+
                              <input className="form-control my-3 w-75"
                             placeholder="Password"
                             type="password"
-                            name="password"/>
+                            name="password"
+                            value={password}
+                            onChange={onInputChange}/>
                         </div>
                         <div className="modal-footer">
                             <button type="submit" 
