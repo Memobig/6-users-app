9e7c73b6ace4c4093f0b226737b60bdbf6db7665
Memobig <memobig@gmail.com>
2024-05-22 14:48:58 -0600

    estructurando login y logout en un hook useaAuth

--- src/UsersApp.jsx
+++ src/UsersApp.jsx
@@ -1,39 +1,12 @@
 import { useReducer, useState } from "react";
 import { LoginPages } from "./auth/pages/LoginPage";
-import { UsersPage } from "./pages/UserPages";
-import { loginReducer } from "./auth/reducers/loginReducer";
-import Swal from "sweetalert2";
+import { UsersPage } from "./pages/UserPages"
 import { Navbar } from "./components/layout/Navbar";
-
-const initialLogin = JSON.parse(sessionStorage.getItem('login')) || {
-    isAuth: false,
-    user: undefined,
-}
+import { useAuth } from "./auth/hooks/useAuth";
 
 export const UsersApp = () => {
 
-    const [login, dispatch] = useReducer(loginReducer, initialLogin);
-    const handlerLogin = ({ username, password }) => {
-        if (username === 'admin' && password === '12345') {
-            const user = { username: 'admin' };
-            dispatch({
-                type: 'login',
-                payload: user,
-            });
-            sessionStorage.setItem('login', JSON.stringify({
-                isAuth: true,
-                user,
-            }));
-        } else {
-            Swal.fire("Error login", 'Username o password invalidos', 'error');
-        }
-    }
-    const handlerLogout = () => {
-        dispatch({
-            type: 'logout',
-        });
-        sessionStorage.removeItem('login');
-    }
+    const {login, handlerLogin, handlerLogout} = useAuth();    
 
     return (
         <>
